f8fdbf4fb75ab1c39a1d59efdd1bd603fe6421c0
victorvuelma <victorvuelma@outlook.com>
2019-04-14 17:57:00 -0300

    Create endpoint for add food ingredient

--- src/main/java/me/vuelma/favoritefoods/food/FoodController.java
+++ src/main/java/me/vuelma/favoritefoods/food/FoodController.java
@@ -88,4 +88,26 @@ public class FoodController {
         return ResponseEntity.ok(food.get().getIngredients());
     }
 
+    @PutMapping("/{f_id}/ingredients/{i_id}")
+    public ResponseEntity<List<Ingredient>> addFoodIngredient(@PathVariable("f_id") long foodId,
+                                                              @PathVariable("i_id") long ingredientId){
+        Optional<Food> findFood =  foodRepository.findById(foodId);
+        if(!findFood.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+
+        Optional<Ingredient> findIngredient =  ingredientRepository.findById(ingredientId);
+        if(!findIngredient.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+
+        Food food = findFood.get();
+        Ingredient ingredient = findIngredient.get();
+        if(!food.getIngredients().contains(ingredient)){
+            food.getIngredients().add(ingredient);
+            food = foodRepository.save(food);
+        }
+        return ResponseEntity.ok(food.getIngredients());
+    }
+
 }
